f4fd76e704b64d0e6dc4315d46e183944680b255
ezio84 <brabus84@gmail.com>
2017-03-31 11:47:28 +0200

    SmartBar: allow set custom buttons opacity when Pulse is active [1/3]
    nowadays with navbar themes the user can use custom navbar buttons
    with particular design (e.g. an empty square with just corners),
    in those cases he could need to keep bigger opacity value when pulse is
    active otherwise buttons visibility would be compromised.
    
    Change-Id: I388cc3cf556bfb3378adec8908a5dfe59338fc4e

--- src/com/android/systemui/navigation/smartbar/SmartBarView.java
+++ src/com/android/systemui/navigation/smartbar/SmartBarView.java
@@ -73,8 +73,6 @@ import java.util.Set;
 public class SmartBarView extends BaseNavigationBar {
     final static boolean DEBUG = false;
     final static String TAG = SmartBarView.class.getSimpleName();
-    final static float PULSE_ALPHA_FADE = 0.3f; // take bar alpha low so keys are vaguely visible
-                                                // but not intrusive during Pulse
     final static int PULSE_FADE_OUT_DURATION = 250;
     final static int PULSE_FADE_IN_DURATION = 200;
 
@@ -89,6 +87,7 @@ public class SmartBarView extends BaseNavigationBar {
         sUris.add(Settings.Secure.getUriFor("smartbar_ime_hint_mode"));
         sUris.add(Settings.Secure.getUriFor("smartbar_button_animation_style"));
         sUris.add(Settings.Secure.getUriFor(Settings.Secure.NAVBAR_BUTTONS_ALPHA));
+        sUris.add(Settings.Secure.getUriFor(Settings.Secure.PULSE_CUSTOM_BUTTONS_OPACITY));
     }
 
     private SmartObservable mObservable = new SmartObservable() {
@@ -108,6 +107,8 @@ public class SmartBarView extends BaseNavigationBar {
                 updateAnimationStyle();
             } else if (uri.equals(Settings.Secure.getUriFor(Settings.Secure.NAVBAR_BUTTONS_ALPHA))) {
                 updateButtonAlpha();
+            } else if (uri.equals(Settings.Secure.getUriFor(Settings.Secure.PULSE_CUSTOM_BUTTONS_OPACITY))) {
+                updatePulseNavButtonsOpacity();
             }
         }
     };
@@ -125,6 +126,7 @@ public class SmartBarView extends BaseNavigationBar {
     private int mImeHintMode;
     private int mButtonAnimationStyle;
     private float mCustomAlpha;
+    public float mPulseNavButtonsOpacity;
 
     private AudioManager mAudioManager;
     private MediaMonitor mMediaMonitor;
@@ -487,6 +489,7 @@ public class SmartBarView extends BaseNavigationBar {
         setMenuVisibility(mShowMenu, true);
         setNavigationIconHints(mNavigationIconHints, true);
         setButtonAlpha();
+        updatePulseNavButtonsOpacity();
     }
 
     private void updateContextLayoutSettings() {
@@ -688,6 +691,17 @@ public class SmartBarView extends BaseNavigationBar {
         }
     }
 
+    private void updatePulseNavButtonsOpacity() {
+        mPulseNavButtonsOpacity = alphaIntToFloat(Settings.Secure.getIntForUser(getContext().getContentResolver(),
+                Settings.Secure.PULSE_CUSTOM_BUTTONS_OPACITY, 200, UserHandle.USER_CURRENT));
+        if (isBarPulseFaded()) {
+            final View currentNavButtons = getCurrentView().findViewWithTag(Res.Common.NAV_BUTTONS);
+            final View hiddenNavButtons = getHiddenView().findViewWithTag(Res.Common.NAV_BUTTONS);
+            currentNavButtons.setAlpha(mPulseNavButtonsOpacity);
+            hiddenNavButtons.setAlpha(mPulseNavButtonsOpacity);
+        }
+    }
+
     @Override
     public boolean onStartPulse(Animation animatePulseIn) {
         if (mEditor != null && mEditor.getMode() == BaseEditor.MODE_ON) {
@@ -695,7 +709,7 @@ public class SmartBarView extends BaseNavigationBar {
         }
         final View currentNavButtons = getCurrentView().findViewWithTag(Res.Common.NAV_BUTTONS);
         final View hiddenNavButtons = getHiddenView().findViewWithTag(Res.Common.NAV_BUTTONS);
-        final float fadeAlpha = Math.min(mCustomAlpha, PULSE_ALPHA_FADE);
+        final float fadeAlpha = mPulseNavButtonsOpacity;
 
         // no need to animate the GONE view, but keep alpha inline since onStartPulse
         // is a oneshot call
